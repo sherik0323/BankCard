4dbf68ddb44118b4a5228853ebe463ed785e869f
Sherozbek <sherozradjabov.2004@gmail.com>
2023-11-23 21:48:33 +0500

    Initial Commit

---
+++ bankcard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bankcard
+//
+//  Created by Sherozbek on 23/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
